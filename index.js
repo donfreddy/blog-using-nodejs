dfb945b408569492218f19f5e0f23668128e354a
Donfreddy <freddytamwo@gmail.com>
2020-07-13 21:00:43 -0800

    Mysql added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+const mysql = require("mysql");
 const fileUpload = require("express-fileupload");
 
 const routes = require("./routes/blog-router.js");
@@ -10,13 +11,10 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-const storePost = require('./middleware/storePost')
-app.use('/posts/store', storePost)
-
 app.use(fileUpload());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/public', express.static("public"));
+app.use("/public", express.static("public"));
 
 app.use(routes);
 
@@ -24,11 +22,32 @@ app.listen(8080, () => {
   console.log(`Allez à cette adresse: http://localhost:${PORT}`);
 });
 
-/* Connect to MongoDB */
-mongoose
+// Create connection to database
+const db = mysql.createConnection({
+  connectionLimit: 100,
+  host: "localhost",
+  user: "root",
+  password: "freddy5050",
+  database: "blog_app",
+  debug: false,
+});
+
+// Connect to Mysql
+db.connect((err) => {
+  if (err) {
+    console.error("Quelque chose a mal tourné");
+    throw err;
+  }
+  console.log("Vous êtes maintenant connecté à Mysql !");
+});
+global.db = db;
+
+// Connect to MongoDB
+/* mongoose
   .connect("mongodb://localhost:27017/blog-app", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log("Vous êtes maintenant connecté à Mongo!"))
   .catch((err) => console.error("Quelque chose a mal tourné", err));
+ */
--- routes/blog-router.js
+++ routes/blog-router.js
@@ -1,19 +1,96 @@
 const express = require("express");
 const router = express.Router();
-const Post = require("../database/models/post");
+const fs = require("fs");
 
-router.get("/", async (req, res) => {
-  const posts = await Post.find({});
-  res.render("pages/home", { posts: posts });
+router.get("/", (req, res) => {
+  let query = "SELECT * FROM  blog_app.posts ORDER BY id ASC";
+
+  db.query(query, (err, rows) => {
+    if (err) throw err;
+    console.log("The data from posts table are: \n", rows);
+    res.render("pages/home", { posts: rows });
+  });
 });
 
 router.get("/about", (req, res) => {
   res.render("pages/about");
 });
 
-router.get("/post/:id", async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render("pages/post", { post: post });
+router.get("/post/:id", (req, res) => {
+  let postId = req.params.id;
+  let query = "SELECT * FROM `posts` WHERE id = '" + postId + "' ";
+
+  db.query(query, (err, result) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.render("pages/post", { post: result[0] });
+  });
+});
+
+router.get("/edit/:id", (req, res) => {
+  let postId = req.params.id;
+  let query = "SELECT * FROM `posts` WHERE id = '" + postId + "' ";
+
+  db.query(query, (err, result) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.render("pages/edit", { post: result[0] });
+  });
+});
+
+router.post("/edit/:id", (req, res) => {
+  let postId = req.params.id;
+  let username = req.body.username;
+  let title = req.body.title;
+  let description = req.body.description;
+  let content = req.body.content;
+
+  let query =
+    "UPDATE `posts` SET `user_name` = '" +
+    username +
+    "', `title` = '" +
+    title +
+    "', `description` = '" +
+    description +
+    "', `content` = '" +
+    content +
+    "' WHERE `posts`.`id` = '" +
+    postId +
+    "'";
+
+  db.query(query, (err, result) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.redirect("/");
+  });
+});
+
+router.get("/delete/:id", (req, res) => {
+  let postId = req.params.id;
+  let getImageQuery = 'SELECT image from `posts` WHERE id = "' + postId + '"';
+  let deletePostQuery = 'DELETE FROM posts WHERE id = "' + postId + '"';
+
+  db.query(getImageQuery, (err, result) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    let image = result[0].image;
+
+    fs.unlink(`public/posts/${image}`, (err) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      db.query(deletePostQuery, (err, result) => {
+        if (err) {
+          return res.status(500).send(err);
+        }
+        res.redirect("/");
+      });
+    });
+  });
 });
 
 router.get("/contact", (req, res) => {
@@ -25,19 +102,48 @@ router.get("/posts/new", (req, res) => {
 });
 
 router.post("/posts/store", (req, res) => {
-  const { image } = req.files;
+  if (!req.files) {
+    return res.status(400).send("No files were uploaded.");
+  }
 
-  image.mv(path.resolve(__dirname, "public/posts", image.name), (error) => {
-    Post.create(
-      {
-        ...req.body,
-        image: `/posts/${image.name}`,
-      },
-      (error, post) => {
-        res.redirect("/");
+  let username = req.body.username;
+  let title = req.body.title;
+  let description = req.body.description;
+  let content = req.body.content;
+  let createdAt = new Date().toISOString().slice(0, 19).replace("T", " ");
+  let uploadedFile = req.files.image;
+  let image_name = uploadedFile.name;
+  let fileExtension = uploadedFile.mimetype.split("/")[1];
+
+  // check the filetype before uploading it
+  if (
+    uploadedFile.mimetype === "image/png" ||
+    uploadedFile.mimetype === "image/jpeg" ||
+    uploadedFile.mimetype === "image/gif"
+  ) {
+    // upload the file to the /public/assets/img directory
+    uploadedFile.mv(`public/posts/${image_name}`, (err) => {
+      if (err) {
+        return res.status(500).send(err);
       }
-    );
-  });
+
+      // send posts to the database
+      let query = `INSERT INTO posts (user_name, title, description, content, image, createdAt) VALUES ('${username}', '${title}', '${description}', '${content}', '${image_name}', '${createdAt}')`;
+
+      // execute query
+      db.query(query, (err, result) => {
+        if (err) {
+          return res.status(500).send(err);
+        }
+        res.redirect("/");
+      });
+    });
+  } else {
+    let message =
+      "Invalid File format. Only 'gif', 'jpeg' and 'png' images are allowed.";
+
+    res.render("pages/create", { message: message });
+  }
 });
 
 module.exports = router;
